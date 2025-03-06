f5d33b7301db732dbfab221ff83a647c45900de6
Nehoray <nhoray5@gmail.com>
2025-03-06 20:33:04 +0200

    new terraform project

--- terraform/Main.tf
+++ terraform/Main.tf
@@ -2,9 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# Retrieve the default VPC (required for the security groups)
 data "aws_vpc" "default" {
   default = true
 }
-
-# (Other resources will be added in separate files)
--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,14 +1,16 @@
-# ec2.tf
-resource "aws_key_pair" "my_jenkins_key" {
+resource "aws_key_pair" "jenkins_key" {
   key_name   = "jenkins-key"
   public_key = var.jenkins_public_key
 }
 
 resource "aws_instance" "linux_docker" {
-  ami                   = "ami-08c40ec9ead489470"   # Make sure this is valid for your region
+  ami                   = "ami-08c40ec9ead489470"   # Ensure this AMI is valid in your region
   instance_type         = "t2.micro"
   key_name              = aws_key_pair.jenkins_key.key_name
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+
+  vpc_security_group_ids = [
+    aws_security_group.instance_sg.id
+  ]
 
   user_data = <<-EOF
     #!/bin/bash
@@ -20,6 +22,6 @@ resource "aws_instance" "linux_docker" {
   EOF
 
   tags = {
-    Name = "LinuxDockerInstance1"
+    Name = "LinuxDockerInstance"
   }
 }
