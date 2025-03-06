d30ab629d064dc8b5658547d75250b4bab9d52f4
Nehoray <nhoray5@gmail.com>
2025-03-06 19:57:02 +0200

    new terraform project

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,13 +2,3 @@ variable "jenkins_public_key" {
   description = "Public key for the Jenkins key pair"
   type        = string
 }
-
-variable "jenkins_public_key" {
-  description = "Public key for the Jenkins key pair"
-  type        = string
-}
-
-resource "aws_key_pair" "jenkins_key" {
-  key_name   = "jenkins-key"
-  public_key = var.jenkins_public_key
-}
